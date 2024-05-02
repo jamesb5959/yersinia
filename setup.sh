1017c8bd2ac94a5c9a79ba929f45b643fbda47f4
Bradley James David Beltran <30552360+jamesb5959@users.noreply.github.com>
2024-05-02 09:31:23 -0600

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,9 +5,6 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-cp lib/libnet.so.9.0.0 /usr/lib/libnet.so.9
-cp lib/libnet.so.9.0.0 /usr/lib/libnet.so
-
 ./autogen.sh
 ./configure --disable-gtk
 make
